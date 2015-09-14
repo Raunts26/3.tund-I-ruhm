a818bd5451793a29e0e84c624e6f59771c4fd905
Rauno Kosula <raunkos@greeny.cs.tlu.ee>
2015-09-14 13:47:52 +0300

    Lisatud menüü ifid

--- menu.php
+++ menu.php
@@ -1,10 +1,17 @@
 <h3>Menüü</h3>
 
 <ul>
-	<li>
-		<a href="home.php">Avaleht</a>
-	</li>
-		<li>
-		<a href="login.php">Logi sisse</a>
-	</li>
+	<?php if($page_file != "home.php") { ?>
+		<li><a href="home.php">Avaleht</a></li>
+	<?php } else { ?>
+		<li>Avaleht</li>
+	<?php } ?>
+	
+	<?php if($page_file != "login.php") { ?>
+		<li><a href="login.php">Logi sisse</a></li>
+	<?php } else { ?>
+		<li>Logi sisse</li>
+	<?php } ?>
+	
+	
 </ul>
