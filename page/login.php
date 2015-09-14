41ed8a22e32694318929948f6ab0dd436cfa519e
Rauno Kosula <raunkos@greeny.cs.tlu.ee>
2015-09-14 14:27:34 +0300

    ...

--- page/login.php
+++ page/login.php
@@ -20,13 +20,15 @@
 	
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		//isset uurib kas muutuja on loodud ning ega poleks olematu/kehtetu
-		//Uurib, mis nuppu on vajutatud, antul juhul logi sisse
+		//Uurib, mis nuppu on vajutatud, antul juhul logi sissem $_POST["login"]
 		if (isset($_POST["btnlogin"])) {
 		//Kontrolli kasutaja e-posti ja parooli, et see poleks tühi
 			if (empty($_POST["email"])) {
 				$email_error = "Palun sisesta e-posti aadress!";
 			
-			}	
+			} else {
+				$email = $_POST["email"];
+			}
 			if (empty($_POST["password"])) {
 				$password_error = "Palun sisesta parool!";
 			
@@ -35,8 +37,6 @@
 	}
 	
 	//Registreerumise errorid
-	
-	
 	$reg_email_error = "";
 	$reg_password_error = "";
 	$reg_password_repeat_error = "";
@@ -67,20 +67,37 @@
 				
 			}
 		}
-	}
+	} 
+	
+	//Reg Errorid lõpp
+	
+	//Muutujad väärtustega
+	
+	$email = "";
 	
 	
 ?>
 <?php require_once("../header.php"); ?>
-		<table>
-			<td>
 				<div id="login">
-					<form action="userform.php" method="post">
+					<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
 						<h2>Logi sisse</h2>
-						<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?><br><br>
+						<input name="email" type="email" placeholder="E-post" value="<?php echo $email; ?>"> <?php echo $email_error; ?><br><br>
 						<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?><br><br>
 						
 						<input type="submit" name="btnlogin" value="Logi sisse">
 					</form>
+				</div>
+
+				<div id="register">
+					<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+						<h2>Registreeru</h2>
+						<input name="reg_email" type="email" placeholder="E-post" value="<?php echo $email; ?>">* <?php echo $reg_email_error; ?><br><br>
+						<input name="reg_name" type="text" placeholder="Teie nimi">* <?php echo $reg_name_error; ?><br><br>						
+						<input name="reg_password" type="password" placeholder="Parool">* <?php echo $reg_password_error;?><br><br>
+						<input name="reg_password_repeat" type="password" placeholder="Korda parooli">* <?php echo $reg_password_repeat_error; ?><br><br>
+						
+						<input type="submit" name="btnregister" value="Registreeru">
+					</form>
+				</div>
 
 <?php require_once("../footer.php"); ?>
